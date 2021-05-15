1ca7ceafbad965beb956fc5dfaf144211dc8376d
eddier0 <eddier2k9@gmail.com>
2021-05-15 14:37:15 -0400

    dom4

---
+++ scripts.js
@@ -0,0 +1,8 @@
+<
+  
+function countOptions(){
+  alert("There are "+ document.getElementById("d1").length + "items in the list.");
+  for (i=0;i, 1;i++){
+    alert(document.getElementById("d1"));;
+  }
+}
